3e0e87fda3062678b2ceddaf6e86831621d39b7b
EA Rashel <rashel320@gmail.com>
2017-10-18 23:43:26 +0600

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Estique on 10/18/17.
+//  Copyright © 2017 Estique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
