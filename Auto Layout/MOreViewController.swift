e2f65499e4253a69301ac2fc2faaf63bb36bc48a
EA Rashel <rashel320@gmail.com>
2017-10-20 23:02:51 +0600

    add stack view

---
+++ Auto Layout/MOreViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MOreViewController.swift
+//  Auto Layout
+//
+//  Created by Estique on 10/20/17.
+//  Copyright © 2017 Estique. All rights reserved.
+//
+
+import UIKit
+
+class MOreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
